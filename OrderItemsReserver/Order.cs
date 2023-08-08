b72b88aa41fc2dfc121458c989f2ee204176810d
Ivan Zamkovyi <izamkovyi@gmail.com>
2023-08-08 21:08:02 +0300

    done

---
+++ OrderItemsReserver/Order.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace OrderItemsReserver;
+
+public class Order
+{
+    [JsonProperty(PropertyName = "id")]
+    public string Id { get; set; }
+
+    [JsonProperty(PropertyName = "buyerId")]
+    public string BuyerId { get; set; }
+
+    public Address ShipToAddress { get; set; }
+    public IEnumerable<OrderItem> OrderItems { get; set; }
+    public decimal Total { get; private set; }
+    public Order(string id, string buyerId, Address shipToAddress, IEnumerable<OrderItem> items, decimal total)
+    {
+        Id = $"{buyerId}.{id}";
+        BuyerId = buyerId;
+        ShipToAddress = shipToAddress;
+        OrderItems = items;
+        Total = total;
+    }
+}
+
+public class OrderItem
+{
+    public string Id { get; set; }
+    public CatalogItemOrdered ItemOrdered { get; set; }
+    public decimal UnitPrice { get; set; }
+    public int Units { get; set; }
+}
+
+public class CatalogItemOrdered
+{
+    public int CatalogItemId { get; set; }
+    public string ProductName { get; set; }
+    public string PictureUri { get; set; }
+}
+
+public class Address
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
+    public string ZipCode { get; set; }
+}
--- OrderItemsReserver/OrderItemsReserver.cs
+++ OrderItemsReserver/OrderItemsReserver.cs
@@ -7,39 +7,61 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
-using Azure.Storage.Blobs;
-using System.Text;
+using Microsoft.Azure.Cosmos;
 
 namespace OrderItemsReserver
 {
     public static class OrderItemsReserver
     {
+        private static readonly string EndpointUri = Environment.GetEnvironmentVariable("EndPointUri");
+        private static readonly string PrimaryKey = Environment.GetEnvironmentVariable("PrimaryKey");
+        private static readonly string databaseId = "orders";
+        private static readonly string containerId = "Items";
+
         [FunctionName("OrderItemsReserver")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            string connection = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
-            string containerName = Environment.GetEnvironmentVariable("ContainerName");
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic dataInJson = JsonConvert.DeserializeObject(requestBody);
-            var orderId = dataInJson?.id;
+            var order = JsonConvert.DeserializeObject<Order>(requestBody);
+
             try
             {
-                var blobClient = new BlobContainerClient(connection, containerName);
-                using Stream myBlob =  new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(dataInJson, Formatting.Indented)));
-                var blob = blobClient.GetBlobClient($"{Guid.NewGuid()}.json");
-                await blob.UploadAsync(myBlob);
+                var cosmosClient = new CosmosClient(
+                    EndpointUri, 
+                    PrimaryKey, 
+                    new CosmosClientOptions());
+
+                // Get db
+                var dbResponse = await cosmosClient.CreateDatabaseIfNotExistsAsync(databaseId);
+                var db = dbResponse.Database;
+                Console.WriteLine("Get Database: {0}\n", db.Id);
+
+                // Get container
+                var containerResponse = await db.CreateContainerIfNotExistsAsync(containerId, "/buyerId");
+                var container = containerResponse.Container;
+                Console.WriteLine("Get Container: {0}\n", container.Id);
+
+                // Create an item in the container representing the Andersen family. Note we provide the value of the partition key for this item, which is "Andersen"
+                ItemResponse<Order> orderResponse = await container.CreateItemAsync<Order>(order, new PartitionKey(order.BuyerId));
+
+                // Note that after creating the item, we can access the body of the item with the Resource property off the ItemResponse. We can also access the RequestCharge property to see the amount of RUs consumed on this request.
+                Console.WriteLine("Created item in database with id: {0} Operation consumed {1} RUs.\n", orderResponse.Resource.Id, orderResponse.RequestCharge);
 
             }
-            catch (Exception ex)
+            catch (CosmosException de)
             {
-                log.LogInformation($"error: {ex.Message}");
+                Exception baseException = de.GetBaseException();
+                log.LogInformation($"{de.StatusCode} error occurred: {de}");
+            }
+            catch (Exception e)
+            {
+                log.LogInformation($"error: {e.Message}");
             }
 
 
-            log.LogInformation($"OrderItemsReserver HTTP trigger successfully processed for order {orderId}");
+            log.LogInformation($"OrderItemsReserver HTTP trigger successfully processed for order {order.Id}");
 
             return new OkObjectResult("Func successfully processed");
         }
